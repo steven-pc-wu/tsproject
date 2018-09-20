d775b27dddaa24479f1677497dcb735618d4dc2f
“wuhaifeng” <wuhaifeng@shoyoo.com>
2018-09-20 18:05:06 +0800

    mdy1

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';                     // 测试能否正常使用 Node 的内置模块
-
+import * as _ from 'lodash';
 
 export module Util {
   export const UTIL_VALUE:number = 123;
@@ -11,6 +11,7 @@ export module Util {
  */
 export class NodeModuleTester {
   public static readonly STATIC_VAR = 'STATIC';   // 测试static变量
+  _num: number = 0;
 
   constructor(                                    // 测试构造方法
     private readonly f1: string,
@@ -29,5 +30,63 @@ export class NodeModuleTester {
   }
 
 }
+
+export class Module {
+  SayHi() {
+    console.log("this is module hi");
+
+    console.log(_.escape('fred, barney, & pebbles'));
+
+    let x = _.parseInt('70', 8);
+    console.log(`x = ${x}`);
+
+    var users = [
+      { 'user': 'barney',  'active': true },
+      { 'user': 'fred',    'active': false },
+      { 'user': 'pebbles', 'active': false }
+    ];
+
+    console.log(_.dropRightWhile(users, function(o){
+      return (o.active != true);
+    }));
+  }
+}
+
+/**
+ * 所有单例的基类，做了单例的基础检查。所有子类最好都写一个getInstance的静态方法来获取
+ * @author sodaChen
+ * Date:2012-10-29
+ */
+export class Singleton
+{
+
+  /**
+   *  这个是单例需要进行私有化
+   */
+    private constructor()
+    {
+
+    }
+    //注意，Singleton是要替换成你自己实现的子类 这里没有实际的作用
+    private static instance:Singleton;
+    /**
+     * 获取实例的静态方法实例
+     * @return
+     *
+     */
+    public static getInstance():Singleton
+    {
+        if(!this.instance)
+        {
+            this.instance = new Singleton();
+        }
+        return this.instance;
+    }
+
+    public sayHello() {
+      console.log("helo every one");
+    }
+}
+
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import {Util} from './core/utils';
+import {Util as Ul} from './core/utils';
+// import * as Ul from './core/utils'
 
 /**
  * main 入口
@@ -6,12 +7,18 @@ import {Util} from './core/utils';
  * 测试
  */
 function main() {
-    const tester = new Util.NodeModuleTester("s1", 1);
+    const tester = new Ul.NodeModuleTester("s1", 1);
 
-    console.log(Util.NodeModuleTester.STATIC_VAR);
-    console.log(Util.NodeModuleTester.testPath());
-    console.log("hello steven1122");
+    console.log(Ul.NodeModuleTester.STATIC_VAR);
+    console.log(Ul.NodeModuleTester.testPath());
+    console.log("hello steven112289456123");
     tester.sayHello();
+
+    let mod = new Ul.Module();
+    mod.SayHi();
+
+    Ul.Singleton.getInstance().sayHello();
+
 }
 
 main();
